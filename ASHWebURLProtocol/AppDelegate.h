f56bfc2c783a15e23683ce2d797792902de0e7d1
wushenghua <wushenghua@linggan.com>
2016-11-16 14:53:30 +0800

    INIT

---
+++ ASHWebURLProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASHWebURLProtocol
+//
+//  Created by xmfish on 16/11/16.
+//  Copyright © 2016年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASHWebURLProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASHWebURLProtocol
+//
+//  Created by xmfish on 16/11/16.
+//  Copyright © 2016年 ash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
